6db42782c2f3ca4e7a411c02962368758b3b8537
mongsam2 <songjaehyun121476@gmail.com>
2025-11-11 10:29:32 +0900

    feat: User 모델 email 필드 추가

--- src/users/models.py
+++ src/users/models.py
@@ -1,5 +1,9 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    PermissionsMixin,
+    BaseUserManager,
+)
 
 
 # Create your models here.
@@ -9,6 +13,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     """
 
     username = models.CharField(max_length=30, unique=True)
+    email = models.EmailField()
 
     objects = BaseUserManager()
 
