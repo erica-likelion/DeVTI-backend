a936f9f97e36e829f33804d25c026930453d3cce
임진수 <jsbd0109@naver.com>
2025-11-21 22:18:44 +0900

    refactor: 표준화된 응답 형식에 맞춘 코드 수정

--- src/users/profile/views.py
+++ src/users/profile/views.py
@@ -2,6 +2,7 @@ from rest_framework.views import APIView
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework.exceptions import ValidationError, ParseError, NotFound
 from django.shortcuts import get_object_or_404
 from django.db import transaction
 from rest_framework.permissions import IsAuthenticated
@@ -13,17 +14,15 @@ def participant_profile_view(request):
     return Response("응답 예시")
 
 
-# 쿼리 파라미터와 해당 파트의 serializer/model을 연결
+# 쿼리 파라미터와 해당 파트의 serializer/model/message를 매핑
 PART_DISPATCHER = {
     'PM': {'serializer': ProfilePMCreateSerializer, 'model': ProfilePM, 'message': 'PM 프로필 생성 완료'},
     'FE': {'serializer': ProfileFECreateSerializer, 'model': ProfileFE, 'message': 'FE 프로필 생성 완료'},
     'BE': {'serializer': ProfileBECreateSerializer, 'model': ProfileBE, 'message': 'BE 프로필 생성 완료'},
     'DE': {'serializer': ProfileDECreateSerializer, 'model': ProfileDE, 'message': 'DE 프로필 생성 완료'},
 }
-class ProfileView(APIView):
-    # 로그인된 유저만 접근 가능
-    permission_classes = [IsAuthenticated]
 
+class ProfileView(APIView):
     def get(self, request):
         return Response("응답 예시")
 
@@ -31,50 +30,37 @@ class ProfileView(APIView):
         user = request.user
         part = request.query_params.get('part') # 쿼리 파라미터에서 part값 가져오기
 
+        # 파라미터 값(part) 검사
         if not part or part not in PART_DISPATCHER:
-            return Response(
-                {"message" : "part 쿼리 파라미터가 유요하지 않습니다. (허용 값: PM, FE, BE, DE)"},
-                status = status.HTTP_400_BAD_REQUEST
-            )
+            raise ParseError("part 쿼리 파라미터가 유요하지 않습니다. (허용 값: PM, FE, BE, DE)")
         
+        # dispatcher에서 필요한 정보 추출
         dispatcher = PART_DISPATCHER[part]
         PartModel = dispatcher['model']
         PartSerializer = dispatcher['serializer']
 
-        # 공통 Profile 인스턴스 가져오기
-        try:
-            profile = Profile.objects.get(user_id=user)
-        except Profile.DoesNotExist:
-            return Response(
-                {"message": "공통 프로필이 존재하지 않습니다."}, status=status.HTTP_404_NOT_FOUND
-            )
+        # 공통 Profile 가져오기 (소셜 로그인 시점에 생성)
+        profile = get_object_or_404(Profile, user=user)
         
+        # 이미 해당 part의 프로필이 존재하면 에러 발생 (이미 있으면 PUT 메서드로 수정만 가능)
         if PartModel.objects.filter(profile_id = profile).exists():
-            return Response(
-                {"message": f"이미 해당 파트({part})의 프로필이 존재합니다."},
-                status=status.HTTP_409_CONFLICT
-            )
+            raise ValidationError(f"이미 해당 파트({part})의 프로필이 존재합니다")
 
+        # 데이터 유효성 검사
         serializer = PartSerializer(data = request.data)
         serializer.is_valid(raise_exception=True)
 
         # DB에 저장
-        try:
-            with transaction.atomic():
-                new_part_instance = serializer.save(profile_id = profile)
-        except Exception as e:
-            return Response(
-                {"message": "DB 저장 중 오류 발생.", "error": str(e)}, 
-                status=status.HTTP_500_INTERNAL_SERVER_ERROR
-            )
+        serializer.save(profile_id=profile)
         
-        # 새로운 파트가 추가되었으니 available_parts 목록 재생성
+        # available_parts 목록 재생성(방금 생성된 파트가 포함된 최신 목록을 반환하기 위함)
         available_parts = []
         if ProfilePM.objects.filter(profile_id=profile).exists(): available_parts.append("PM")
         if ProfileFE.objects.filter(profile_id=profile).exists(): available_parts.append("FE")
         if ProfileBE.objects.filter(profile_id=profile).exists(): available_parts.append("BE")
         if ProfileDE.objects.filter(profile_id=profile).exists(): available_parts.append("DE")
 
+        # response data 구성
         response_data = {
             "username": user.username,
             "email": user.email,
@@ -84,17 +70,13 @@ class ProfileView(APIView):
             "part": part,
         }
 
-        # serializer의 결과를 합치기 (파트별 필드)
+        # 파트별 프로필 데이터 병합(response 값을 맞추기 위해)
         response_data.update(serializer.data)
 
-        final_response = {
-            "status" : "success",
-            "code" : status.HTTP_201_CREATED,
-            "data": response_data,
-            "message" : dispatcher['message']
-        }
+        # message를 파트별로 구분해서 response_data에 추가 
+        response_data['message'] = dispatcher['message']
 
-        return Response(final_response, status=status.HTTP_201_CREATED)
+        return Response(response_data, status=status.HTTP_201_CREATED)
 
 
     def put(self, request):
