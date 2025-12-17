33c807709dfd2839d17a4849597ccb27394f2a77
Seochan Moon <moonsc1110@gmail.com>
2025-12-18 04:04:19 +0900

    fix: Participant.role의 max_length 확장

--- src/matchings/models.py
+++ src/matchings/models.py
@@ -16,7 +16,7 @@ class Participant(models.Model):
         "users.User", on_delete=models.CASCADE, db_column="user_id"
     )
     username = models.CharField(max_length=30)
-    role = models.CharField(max_length=10)
+    role = models.CharField(max_length=20)
     part = models.CharField(max_length=10)
     team_vibe = models.CharField(max_length=20)
     active_hours = models.CharField(max_length=10)
